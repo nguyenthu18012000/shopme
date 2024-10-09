115b46c9406f7367595d1a413025ea191476fbb7
nguyenthu <nguyenthu180100@gmail.com>
2024-10-09 17:19:27 +0700

    temp

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,14 +20,15 @@ public class UserController {
 	
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/users")
-	public List<User> listAll(Model model) {
+	public ResponseEntity<Object> listAll(Model model) {
 		List<User> listUsers = service.listAll();
 //		model.addAttribute("listUsers", listUsers);
-		return listUsers;
+		return ResponseEntity.ok(listUsers);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/users/new")
-	public String newUser() {
-		return "user_form";
+	public ResponseEntity<Object> newUser() {
+		return ResponseEntity.ok("aaaaaa");
 	}
 }
